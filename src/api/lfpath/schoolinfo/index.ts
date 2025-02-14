7e3fb666254c1ad248e239d12de2c37ea7a67ea8
lifxue <lifxue@gmail.com>
2025-02-14 21:44:34 +0800

    feat(lfpath): 添加高校信息管理功能
    - 新增高校信息相关的字典类型
    - 实现高校信息的 CRUD API 接口
    - 开发高校信息的列表页面和表单组件
    -集成国际化和消息提示功能

---
+++ src/api/lfpath/schoolinfo/index.ts
@@ -0,0 +1,45 @@
+import request from '@/config/axios'
+
+// 高校信息 VO
+export interface SchoolInfoVO {
+  id: number // 编号
+  schoolName: string // 学校名称
+  schoolCode: string // 学校标识码
+  administrativeDepartment: string // 管理部门
+  location: string // 所在地
+  educationLevel: string // 办学层次
+  educationSystem: string // 办学体制
+}
+
+// 高校信息 API
+export const SchoolInfoApi = {
+  // 查询高校信息分页
+  getSchoolInfoPage: async (params: any) => {
+    return await request.get({ url: `/lfpath/school-info/page`, params })
+  },
+
+  // 查询高校信息详情
+  getSchoolInfo: async (id: number) => {
+    return await request.get({ url: `/lfpath/school-info/get?id=` + id })
+  },
+
+  // 新增高校信息
+  createSchoolInfo: async (data: SchoolInfoVO) => {
+    return await request.post({ url: `/lfpath/school-info/create`, data })
+  },
+
+  // 修改高校信息
+  updateSchoolInfo: async (data: SchoolInfoVO) => {
+    return await request.put({ url: `/lfpath/school-info/update`, data })
+  },
+
+  // 删除高校信息
+  deleteSchoolInfo: async (id: number) => {
+    return await request.delete({ url: `/lfpath/school-info/delete?id=` + id })
+  },
+
+  // 导出高校信息 Excel
+  exportSchoolInfo: async (params) => {
+    return await request.download({ url: `/lfpath/school-info/export-excel`, params })
+  },
+}
